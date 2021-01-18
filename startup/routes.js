cfa1a89baa6944557e92de93dbbe4a3068742dd5
Ismaeil Ghouneim <Is.ghouneim@gmail.com>
2021-01-18 23:23:11 +0200

    Star test

--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
     app.use(function (req, res, next) {
 
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'https://poemer.herokuapp.com');
+        res.setHeader('Access-Control-Allow-Origin', '*');
     
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
